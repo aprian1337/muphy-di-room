d8a423fe5f1d0f4a4618edf3d21b3d53edc460bd
aprian1337 <dwikyapriyan@gmail.com>
2021-05-07 02:43:59 +0700

    adding DetailMovieTv model

---
+++ app/src/main/java/com/aprian1337/movie_catalogue/data/models/DetailMovieTv.kt
@@ -0,0 +1,10 @@
+package com.aprian1337.movie_catalogue.data.models
+
+data class DetailMovieTv(
+    val title: String,
+    val genre: String,
+    val release: String,
+    val overview: String,
+    val image: String,
+    val popularity: String
+)
